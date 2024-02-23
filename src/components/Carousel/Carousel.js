41e644b35524e4418c9a50a0915502e1a1c05974
Emiliano <emilianodebenedetti@outlook.com>
2024-02-23 15:14:00 -0300

    Agrego componente Carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,16 @@
+import React from 'react'
+/* import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules'; */
+
+const Carousel = () => {
+  return (
+    <div className='bg-red min-h-44 h-auto z-1 flex items-center justify-center shadow-lg'>
+        <img alt='Logo' className='max-h-28' src="/logo.jpeg" img/>
+    </div>
+  )
+}
+
+export default Carousel
