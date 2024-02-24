5b7340136902f615eccb5a5d224571f3c16f4c47
Emiliano <emilianodebenedetti@outlook.com>
2024-02-24 10:29:55 -0300

    Agrego componente Slogan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Carousel from './components/Carousel/Carousel';
 import Navbar from './components/Navbar/Navbar';
+import Slogan from './components/Slogan/Slogan';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Carousel />
+        <Slogan />
       </header>
     </div>
   );
---
+++ src/components/Slogan/Slogan.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Slogan = () => {
+  return (
+    <div className='grid place-content-center pt-2'>
+      <img className='w-32 mx-auto' src="/logo-navbar.jpeg" alt='Logo' img/>
+      <p className='mx-auto'>Desde 20:00hs hasta 00:30hs</p>
+    </div>
+  )
+}
+
+export default Slogan
